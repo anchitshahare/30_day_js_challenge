5bad378f3f47c055919886a27e05380ba63cc891
anchitshahare <arshahare1310@gmail.com>
2024-07-16 22:15:58 +0530

    day1  challenge

---
+++ day1.js
@@ -0,0 +1,25 @@
+var num = 10;
+console.log(num);
+
+let str = 'string'
+console.log(str);
+
+const bool = true
+console.log(bool);
+
+let arr = new Array(3);
+var obj = {}
+
+console.log("num: ", typeof num)
+console.log("str: ", typeof str)
+console.log("bool: ", typeof bool)
+console.log("arr: ", typeof arr)
+console.log("obj: ", typeof obj)
+
+let res = 1;
+console.log("res: ", res)
+res = 2
+console.log("res: ", res)
+
+bool = false
+console.log("bool: ", bool)

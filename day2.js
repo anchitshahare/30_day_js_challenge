6c33be575a8a50612ba480b10d1a7421609f65ad
anchitshahare <arshahare1310@gmail.com>
2024-07-17 16:36:32 +0530

    day 2 challenge

---
+++ day2.js
@@ -0,0 +1,33 @@
+console.log("2+3 = ", 2+3)
+console.log("2-3 = ", 2-3)
+console.log("2*3 = ", 2*3)
+console.log("2/3 = ", 2/3)
+console.log("2%3 = ", 2%3)
+
+var num = 10
+console.log("num: ", num)
+console.log("num += 3", num += 3)
+console.log("num -= 3", num -= 3)
+
+console.log("3>4 = ", 3>4)
+console.log("3<4 = ", 3<4)
+console.log("3>=4 = ", 3>=4)
+console.log("3<=4 = ", 3<=4)
+console.log("3==4 = ", 3==4)
+console.log("3===4 = ", 3===4)
+
+console.log("4<4 = ", 4<4)
+console.log("4>4 = ", 4>4)
+console.log("4>=4 = ", 4>=4)
+console.log("4<=4 = ", 4<=4)
+console.log("4==4 = ", 4==4)
+console.log("4===4 = ", 4===4)
+
+console.log("3<4 && 4<4 = ", 3<4 && 4<4)
+console.log("3<4 || 4<4 = ", 3<4 || 4<4)
+console.log("!3<4 = ", !(3<4))
+
+num = 3
+console.log(`${num} number is `, num>0 ? 'positive' : 'negative')
+num = -3
+console.log(`${num} number is `, num>0 ? 'positive' : 'negative')
